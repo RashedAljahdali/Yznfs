e4197e5afd764d9d2cfa2f8e56c3e959ac8ee458
slut <shsyge555@gmail.com>
2025-02-27 04:33:48 +0300

    Added the init for the db

--- server/src/init/mod.rs
+++ server/src/init/mod.rs
@@ -9,7 +9,7 @@ if std::fs::exists("init.init").unwrap(){
         std::fs::create_dir_all("files").unwrap();
         std::fs::create_dir_all("data").unwrap();
         std::fs::write("init.init", "1").unwrap();
-
+         initdb();
     }
     else {
         return;
@@ -19,4 +19,32 @@ if std::fs::exists("init.init").unwrap(){
         std::fs::write("init.init" , "0").unwrap();
         init()
     }
+}
+//This function initialize all the tables in our database
+//note I'm using .unwarp() that means this function shall not fail or the program will panic
+fn initdb(){
+    let mut temp = rusqlite::Connection::open_with_flags("data/db" ,
+                                                         rusqlite::OpenFlags::SQLITE_OPEN_CREATE
+                                                             | rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE).unwrap();
+    temp.execute("PRAGMA foreign_keys = ON;", ()).unwrap();
+    temp.execute(r#"CREATE TABLE user (
+    id TEXT PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP)
+    "# , ()).unwrap();
+    temp.execute(r#"CREATE TABLE files (
+    Filename TEXT PRIMARY KEY,
+    owner TEXT NOT NULL,
+    type TEXT,
+    description TEXT,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (owner) REFERENCES user(id) ON DELETE CASCADE)
+    "# , ()).unwrap();
+    temp.execute(r#"CREATE TABLE sessions (
+    owner TEXT NOT NULL,
+    session_id TEXT NOT NULL,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (owner) REFERENCES user(id) ON DELETE CASCADE)
+    "# , ()).unwrap();
 }
